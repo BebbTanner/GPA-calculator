6866d0a83f225b64bae735f6532e9fff2fdccac3
Tanner Bebb <17tbebb@gmail.com>
2025-01-17 11:33:38 -0800

    Added some comments at the top that is a rough outline of what I am trying to accomplish.

--- GPA calculator/Main.cpp
+++ GPA calculator/Main.cpp
@@ -1,7 +1,14 @@
 /*
 GPA calculator
 
-
+input:
+Class name
+	This should be put into an array that can be sized by the user(I think this was a dynmaic array)
+Class grade
+	This should be attached to the items that are stored in the array(Store this as the double data type)
+output:
+GPA of the inputed class grades on the 4.0 scale.
+	Store this as the double data type
 */
 
 #include <iostream>
